822153fb1fc0139aa86ce1fcd3cd6f80fb6c8c18
hikaru19991117 <hika4852@gmail.com>
2023-09-03 10:28:48 +0900

    依存関係

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
-
-from array import array
 import os
 from PIL import Image
-import sys
-import time
 
 subscription_key = os.environ["VISION_KEY"]
 endpoint = os.environ["VISION_ENDPOINT"]
